1fd3dff8d87c9b4f2dc0116a917cdb24eb9569a3
SebLOURDEL <seb.lourdel8@gmail.com>
2023-10-31 14:58:24 +0100

    link correction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import About from './About';
 import Portfolio from './Portfolio';
 import Projects from './Projects';
