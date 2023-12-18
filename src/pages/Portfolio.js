de5c536b8cc037a6975d15f001a785ec9c4ede80
SebLOURDEL <seb.lourdel8@gmail.com>
2023-12-18 15:50:57 +0100

    scrollskills correction

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -140,59 +140,8 @@ function Portfolio() {
                   </div>
                 </div>
               </div>
-              <ScrollSkills />
               <div className="infos imgdef">
-                <div class="image-container">
-                  <div className="overlist"></div>
-                  <div className="overlist overight"></div>
-                  <div class="image-list">
-                    <div>
-                      <img src="./images/git.png" alt="logo wordpress" />
-                    </div>
-                    <div>
-                      <img src="./images/js.png" alt="logo javascript" />
-                    </div>
-                    <div>
-                      <img src="./images/reactlogo.png" alt="logo tagmanager" />
-                    </div>
-                    <div>
-                      <img src="./images/seo.png" alt="logo seo" />
-                    </div>
-                    <div>
-                      <img src="./images/git.png" alt="logo git" />
-                    </div>
-                    <div>
-                      <img src="./images/wp.png" alt="logo wordpress" />
-                    </div>
-                    <div>
-                      <img src="./images/photoshop.png" alt="logo javascript" />
-                    </div>
-                    <div>
-                      <img src="./images/figma.png" alt="logo wordpress" />
-                    </div>
-                    <div>
-                      <img
-                        src="./images/tagmanager.webp"
-                        alt="logo javascript"
-                      />
-                    </div>
-                    <div>
-                      <img src="./images/reactlogo.png" alt="logo tagmanager" />
-                    </div>
-                    <div>
-                      <img src="./images/seo.png" alt="logo seo" />
-                    </div>
-                    <div>
-                      <img src="./images/git.png" alt="logo git" />
-                    </div>
-                    <div>
-                      <img src="./images/wp.png" alt="logo wordpress" />
-                    </div>
-                    <div>
-                      <img src="./images/js.png" alt="logo javascript" />
-                    </div>
-                  </div>
-                </div>
+                <ScrollSkills />
               </div>
             </div>
           </div>
