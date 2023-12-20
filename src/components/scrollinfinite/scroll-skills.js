e9246ea539dac9ce99ef09d90b389240ae1b78b4
SebLOURDEL <seb.lourdel8@gmail.com>
2023-12-20 10:43:31 +0100

    fix path scrollskills image

--- src/components/scrollinfinite/scroll-skills.js
+++ src/components/scrollinfinite/scroll-skills.js
@@ -14,85 +14,85 @@ function ScrollSkills({ applyBorderRadius }) {
       <div class="image-list">
         <div>
           <img
-            src={process.env.PUBLIC_URL + "./images/git.png"}
+            src={process.env.PUBLIC_URL + "/images/git.png"}
             alt="logo wordpress"
           />
         </div>
         <div>
           <img
-            src={process.env.PUBLIC_URL + "./images/js.png"}
+            src={process.env.PUBLIC_URL + "/images/js.png"}
             alt="logo javascript"
           />
         </div>
         <div>
           <img
-            src={process.env.PUBLIC_URL + "./images/reactlogo.png"}
+            src={process.env.PUBLIC_URL + "/images/reactlogo.png"}
             alt="logo tagmanager"
           />
         </div>
         <div>
           <img
-            src={process.env.PUBLIC_URL + "./images/seo.png"}
+            src={process.env.PUBLIC_URL + "/images/seo.png"}
             alt="logo seo"
           />
         </div>
         <div>
           <img
-            src={process.env.PUBLIC_URL + "./images/git.png"}
+            src={process.env.PUBLIC_URL + "/images/git.png"}
             alt="logo git"
           />
         </div>
         <div>
           <img
-            src={process.env.PUBLIC_URL + "./images/wp.png"}
+            src={process.env.PUBLIC_URL + "/images/wp.png"}
             alt="logo wordpress"
           />
         </div>
         <div>
           <img
-            src={process.env.PUBLIC_URL + "./images/photoshop.png"}
+            src={process.env.PUBLIC_URL + "/images/photoshop.png"}
             alt="logo javascript"
           />
         </div>
         <div>
           <img
-            src={process.env.PUBLIC_URL + "./images/figma.png"}
+            src={process.env.PUBLIC_URL + "/images/figma.png"}
             alt="logo wordpress"
           />
         </div>
         <div>
           <img
-            src={process.env.PUBLIC_URL + "./images/tagmanager.webp"}
+            src={process.env.PUBLIC_URL + "/images/tagmanager.webp"}
             alt="logo javascript"
           />
         </div>
         <div>
           <img
-            src={process.env.PUBLIC_URL + "./images/reactlogo.png"}
+            src={process.env.PUBLIC_URL + "/images/reactlogo.png"}
             alt="logo tagmanager"
           />
         </div>
         <div>
           <img
-            src={process.env.PUBLIC_URL + "./images/seo.png"}
+            src={process.env.PUBLIC_URL + "/images/seo.png"}
             alt="logo seo"
           />
         </div>
         <div>
           <img
-            src={process.env.PUBLIC_URL + "./images/git.png"}
+            src={process.env.PUBLIC_URL + "/images/git.png"}
             alt="logo git"
           />
         </div>
         <div>
           <img
-            src={process.env.PUBLIC_URL + "./images/wp.png"}
+            src={process.env.PUBLIC_URL + "/images/wp.png"}
             alt="logo wordpress"
           />
         </div>
         <div>
           <img
-            src={process.env.PUBLIC_URL + "./images/js.png"}
+            src={process.env.PUBLIC_URL + "/images/js.png"}
             alt="logo javascript"
           />
         </div>
